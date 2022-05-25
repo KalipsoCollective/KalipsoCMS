a5b4f11485eefeccb4b62001b7df905c33d6ff91
halillusion <halillusion@gmail.com>
2022-05-25 15:26:41 +0300

    Admin Footer

--- app/Resources/view/_parts/admin_end.php
+++ app/Resources/view/_parts/admin_end.php
@@ -1,3 +1,36 @@
+			<footer class="mt-auto bg-white text-dark shadow p-4 m-2 rounded">
+				<div class="row justify-content-between">
+					<div class="col-12 col-md-6">
+						<?php echo \KN\Helpers\Base::lang('base.copyright') . ' © ' . date('Y') . ' | ' . \KN\Helpers\Base::lang('base.all_rights_reserved'); ?>
+					</div>
+					<div class="col-12 col-md-6 d-flex">
+						<?php 
+						$languages = \KN\Helpers\Base::config('app.available_languages');
+						if ($languages AND count($languages) > 1) {
+							$currentLang = \KN\Helpers\Base::lang('lang.code');
+						?>
+							<div class="dropdown ms-auto">
+								<a class="btn btn-sm btn-dark dropdown-toggle" href="#" role="button" id="languageChange" data-bs-toggle="dropdown" aria-expanded="false">
+									<?php echo \KN\Helpers\Base::lang('base.language') . ': ' . strtoupper($currentLang); ?>
+								</a>
+								<ul class="dropdown-menu" aria-labelledby="languageChange">
+									<?php
+									foreach ($languages as $lang) {
+										
+										echo '
+										<li>
+											<a class="dropdown-item'.($currentLang == $lang ? ' disabled' : '').'" href="' . $this->request->uri . '?lang='.$lang.'">
+												' . strtoupper($lang) . '
+											</a>
+										</li>';
+									}	?>
+								</ul>
+							</div>
+						<?php
+						}	?>
+					</div>
+				</div>
+			</footer>
 		</div>
 		<script src="<?php echo KN\Helpers\Base::assets('libs/bootstrap/bootstrap.bundle.min.js'); ?>"></script>
 		<script src="<?php echo KN\Helpers\Base::assets('libs/nprogress/nprogress.js'); ?>"></script>
