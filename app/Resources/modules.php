27f7143b7510f848ca3f95ae04b7c1b1e07b6184
halillusion <halillusion@gmail.com>
2022-05-26 01:17:38 +0300

    module file

---
+++ app/Resources/modules.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+	'services' => [
+		'name' => 'module.services',
+		'description' => 'module.services_description',
+		'icon' => 'ti ti-folders',
+		'table' => [
+			'id',
+			'name',
+			'updated_at',
+			'updated_by',
+			'created_at',
+			'created_by'
+		],
+		'routes' = [
+			'en' => '/services/:slug',
+			'tr' => '/hizmetler/:slug'
+		],
+		'inputs' => [
+			'name',
+			'slug',
+			'ff' => 
+			'description',
+			'ss'
+		]
+
+	]
+
+];
