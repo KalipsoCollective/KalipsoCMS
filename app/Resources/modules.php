8d8f5699e4c1caaf1e286ab5d3424f016e018205
halillusion <halillusion@gmail.com>
2022-06-30 17:12:32 +0300

    module updates.

--- app/Resources/modules.php
+++ app/Resources/modules.php
@@ -188,8 +188,8 @@ return [
 				'type' => 'input',
 				'attributes' => [
 					'required' => 'true',
-					'data-kn-input' => Base::base('/management/departments/autocomplete'),
-					'data-kn-autocomplete' => json_encode(['module' => 'pages', 'field' => 'description']),
+					'data-kn-input' => Base::base('/management/pages/autocomplete'),
+					'data-kn-autocomplete' => json_encode(['field' => 'description']),
 				],
 			],
 			'content' => [
